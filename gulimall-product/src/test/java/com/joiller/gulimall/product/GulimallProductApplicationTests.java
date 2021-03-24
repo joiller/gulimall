d20ee4b545cb212ea5526196937a5aa543e676cd
jianghuilai <781225147@qq.com>
2021-03-24 23:57:45 +0800

    basic and then, product.category / product.brand

--- gulimall-product/src/test/java/com/joiller/gulimall/product/GulimallProductApplicationTests.java
+++ gulimall-product/src/test/java/com/joiller/gulimall/product/GulimallProductApplicationTests.java
@@ -21,14 +21,6 @@ class GulimallProductApplicationTests {
 
     @Test
     void upload() throws FileNotFoundException {
-        // yourEndpoint填写Bucket所在地域对应的Endpoint。以华东1（杭州）为例，Endpoint填写为https://oss-cn-hangzhou.aliyuncs.com。
-//        String endpoint = "oss-cn-shanghai.aliyuncs.com";
-//// 阿里云账号AccessKey拥有所有API的访问权限，风险很高。强烈建议您创建并使用RAM用户进行API访问或日常运维，请登录RAM控制台创建RAM用户。
-//        String accessKeyId = "LTAI5tLmJv9oWR8DcR5cUNsS";
-//        String accessKeySecret = "JPpyD3KgMk6BSWyixGzitA3mw0Vfzn";
-//
-//// 创建OSSClient实例。
-//        OSS ossClient = new OSSClientBuilder().build(endpoint, accessKeyId, accessKeySecret);
 
 // 填写本地文件的完整路径。如果未指定本地路径，则默认从示例程序所属项目对应本地路径中上传文件流。
         InputStream inputStream = new FileInputStream("/Users/jianghuilai/Desktop/Screen Shot 2020-06-14 at 9.01.39 PM.png");
